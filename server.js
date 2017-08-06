868a3c2797ab5effec3bf3d8ccf024dd1e2bb229
philippe.beroucry <philippe.beroucry.prestataire@it-ce.fr>
2017-08-06 13:49:40 +0200

    add config heroku

---
+++ server.js
@@ -0,0 +1,34 @@
+/**
+ * Created by philippeberoucry on 06/08/2017.
+ */
+const express = require('express');
+const app = express();
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+    return function (req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(
+                ['https://', req.get('Host'), req.url].join('')
+            );
+        }
+        next();
+    };
+};
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
+app.use(forceSSL);
+
